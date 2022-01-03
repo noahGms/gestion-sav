dbe2f61399540021bb83996b4edd698891e9806d
noahGms <noah@keshin.net>
2022-01-03 15:37:28 +0100

    added base layout + renamed welcome route to home

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\View\View;
+
+class HomeController extends Controller
+{
+    /**
+     * @return View
+     */
+    public function index(): View
+    {
+        return view('home');
+    }
+}
