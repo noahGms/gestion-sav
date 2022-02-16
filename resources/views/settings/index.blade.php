044f98b9fb9949c4eeed2b8c3d3e90aa6fa837c3
noahGms <noah@keshin.net>
2022-02-16 14:52:38 +0100

    removed /settings routes

--- resources/views/settings/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends("layouts.base")
-
-@section("content")
-    <div class="alert alert-primary" role="alert">
-        Vous ne pouvez pas accéder à la page des paramètres sur mobile !
-    </div>
-@endsection
