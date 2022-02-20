be9bdd86bdc86412d85cc166b2241b6c03a2e3cf
noahGms <noah@keshin.net>
2022-02-20 16:15:36 +0100

    disabled foreign key constraints check in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Schema::disableForeignKeyConstraints();
         $this->call([
             StateSeeder::class,
             BrandSeeder::class,
@@ -25,5 +27,6 @@ class DatabaseSeeder extends Seeder
 
             ItemSeeder::class
         ]);
+        Schema::enableForeignKeyConstraints();
     }
 }
