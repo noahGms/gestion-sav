639dfc8e00220591736567b20196644747c67518
noahGms <noah@keshin.net>
2022-04-18 15:35:11 +0200

    installed and setup vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import {createApp} from "vue";
 import App from "./app/App";
 import router from "./app/router";
+import store from "./app/store";
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(store).mount('#app');
